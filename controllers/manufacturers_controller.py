c2bd91cf9f2abd2aa045039b5ebb561b7b86a630
Joshua <jssmiller94@gmail.com>
2021-04-26 16:44:43 +0100

    developed create function for manufacturers info

--- controllers/manufacturers_controller.py
+++ controllers/manufacturers_controller.py
@@ -10,3 +10,12 @@ manufacturers_blueprint = Blueprint("manufacturers", __name__)
 def manufacturers():
     manufacturers = manufacturer_repository.select_all()
     return render_template("manufacturer_info/index.html", manufacturers=manufacturers)
+
+@manufacturers_blueprint.route("/manufacturer_info", methods=["POST"])
+def add_manufacturer():
+    name = request.form["name"]
+    contact_details = request.form["contact_details"]
+    info = request.form["info"]
+    new_manufacturer = Manufacturer(name, contact_details, info)
+    manufacturer_repository.save(new_manufacturer)
+    return redirect("/manufacturer_info")
