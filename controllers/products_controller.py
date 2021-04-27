525875bcf9dc8eedcefab0dfbc9236506c7b97d0
Joshua <jssmiller94@gmail.com>
2021-04-27 15:13:54 +0100

    almost finished the create product function

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -13,3 +13,23 @@ products_blueprint = Blueprint("products", __name__)
 def inventory():
     products = product_repository.select_all()
     return render_template("inventory/index.html", products=products)
+
+#new product
+@products_blueprint.route("/add_product")
+def add_item():
+    manufacturers = manufacturer_repository.select_all()
+    return render_template("inventory/add_product.html", manufacturers=manufacturers)
+
+#create product
+@products_blueprint.route("/add_product", methods=["POST"])
+def add_products():
+    name = request.form["name"]
+    description = request.form["description"]
+    quantity = request.form["quantity"]
+    buying_cost = request.form["buying_cost"]
+    selling_price = request.form["selling_price"]
+    manufacturer_id = request.form["manufacturer"]
+    manufacturer = manufacturer_repository.select(manufacturer_id)
+    new_products = Product(name, description, quantity, buying_cost, selling_price, manufacturer)
+    product_repository.save(new_products)
+    return redirect("/inventory")
