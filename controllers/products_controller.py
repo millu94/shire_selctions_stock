fec88b8d953f1e66c5557a7bcdfed16ccd05ef30
Joshua <jssmiller94@gmail.com>
2021-04-27 12:20:02 +0100

    setup the product repo and controller, almost finished writing the index file for the inventory

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -3,4 +3,13 @@ from flask import Blueprint, Flask, redirect, render_template, request
 from models.manufacturer import Manufacturer
 from models.product import Product
 
-from repositories.manufacturer_repository as manufacturer_repository
+import repositories.manufacturer_repository as manufacturer_repository
+import repositories.product_repository as product_repository
+
+products_blueprint = Blueprint("products", __name__)
+
+#index
+@products_blueprint.route("/inventory")
+def inventory()
+    products = product_repository.select_all()
+    return render_template("inventory/index.html", products=products)
